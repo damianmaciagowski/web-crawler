70fe06a211be4d929125c85baf28dc52d2350293
Damian Maciagowski <damian.maciagowski@stxnext.pl>
2022-06-08 10:52:51 +0200

    moved scrapy item to items.py. Added ItemLoader

--- webcrawler/webcrawler/items.py
+++ webcrawler/webcrawler/items.py
@@ -3,10 +3,30 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
+import unicodedata
 import scrapy
+from itemloaders.processors import TakeFirst, MapCompose, Compose
+from w3lib.html import remove_tags, strip_html5_whitespace
 
 
-class WebcrawlerItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class KomputronikItem(scrapy.Item):
+    computer_name = scrapy.Field(
+        input_processor=MapCompose(remove_tags, strip_html5_whitespace),
+        output_processor=TakeFirst(),
+    )
+    price = scrapy.Field(
+        input_processor=MapCompose(remove_tags, strip_html5_whitespace),
+        output_processor=Compose(
+            lambda v: unicodedata.normalize("NFKD", v[0])
+            .strip()
+            .replace(" ", "")
+        ),
+    )
+    producer = scrapy.Field(
+        input_processor=MapCompose(remove_tags, strip_html5_whitespace),
+        output_processor=Compose(lambda v: v[1]),
+    )
+    processor_type = scrapy.Field(
+        input_processor=MapCompose(remove_tags, strip_html5_whitespace),
+        output_processor=Compose(lambda v: v[1]),
+    )
